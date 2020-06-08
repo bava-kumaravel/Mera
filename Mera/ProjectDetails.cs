9ea6b4f39136e6e627dec64346fe7c94bf13bccc
Bhavatarini Kumaravel <bava@xm.studio>
2020-06-09 01:25:50 +0530

    Add project files.

---
+++ Mera/ProjectDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mera
+{
+    class ProjectDetails
+    {
+        public static bool projectSet = false;
+        public static string projectName = "";
+
+    }
+}
